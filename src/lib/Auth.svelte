42d154a4b57269d1054c8ba009e817738009a697
m r <trash89@laposte.net>
2022-07-14 23:25:05 +0200

    auth

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -3,13 +3,14 @@
 
 	let loading = false;
 	let email;
+	let password;
 
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const { error } = await supabase.auth.signIn({ email });
+			const { user, session, error } = await supabase.auth.signUp({ email, password });
 			if (error) throw error;
-			alert("Check your email for the login link!");
+			console.log(user, session);
 		} catch (error) {
 			alert(error.error_description || error.message);
 		} finally {
@@ -20,16 +21,16 @@
 
 <form class="row flex flex-center" on:submit|preventDefault={handleLogin}>
 	<div class="col-6 form-widget">
-		<h1 class="header">Supabase + Svelte</h1>
-		<p class="description">Sign in via magic link with your email below</p>
+		<h1 class="header">My Books</h1>
 		<div>
 			<input class="inputField" type="email" placeholder="Your email" bind:value={email} />
+			<input class="inputField" type="password" placeholder="Your password" bind:value={password} />
 		</div>
 		<div>
 			<input
 				type="submit"
 				class="button block"
-				value={loading ? "Loading" : "Send magic link"}
+				value={loading ? "Loading" : "SignIn"}
 				disabled={loading}
 			/>
 		</div>
