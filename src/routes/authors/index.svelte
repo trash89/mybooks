887117770018fa5c2f9146e7b14f5120e46c5b1c
m r <trash89@laposte.net>
2022-07-21 22:35:47 +0200

    to continue

--- src/routes/authors/index.svelte
+++ src/routes/authors/index.svelte
@@ -4,9 +4,17 @@
   import { supabase } from "$lib/supabaseClient";
 
   let authors = [];
-  const getAllAuthors = async () => {
+  let searchFirstname, searchLastname;
+  const getAllAuthors = async (pFirstname, pLastname) => {
     try {
-      let { data, error } = await supabase.from("authors").select("*");
+      let query = supabase.from("authors").select("*");
+      if (pFirstname) {
+        query = query.like("firstName", `%${pFirstname}%`);
+      }
+      if (pLastname) {
+        query = query.like("lastName", `%${pLastname}%`);
+      }
+      const { data, error } = await query;
       authors = data;
     } catch (err) {
       console.log(err);
@@ -16,15 +24,38 @@
   onMount(async () => {
     await getAllAuthors();
   });
-  //$: console.log($user);
+  $: getAllAuthors(searchFirstname, searchLastname);
 </script>
 
 <h1>authors</h1>
 <a href="/authors/newAuthor"><i class="fa-solid fa-plus" /></a>
-<i class="fa-solid fa-trash" />
-<i class="fa-solid fa-check" />
-<ul>
-  {#each authors as author (author.id)}
-    <li key={author.id}><a href={`/authors/${author.id}`}><i class="fa-solid fa-pen" /></a> {author.firstName} {author.lastName}</li>
-  {/each}
-</ul>
+
+<table>
+  <thead>
+    <tr>
+      <th>Search</th>
+      <th><input bind:value={searchFirstname} /></th>
+      <th><input bind:value={searchLastname} /></th>
+    </tr>
+    <tr>
+      <th>Action</th>
+      <th>Firstname </th>
+      <th>Lastname</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each authors as author (author.id)}
+      <tr key={author.id}>
+        <td>
+          <a href={`/authors/${author.id}`}><i class="fa-solid fa-pen" /></a>
+        </td>
+        <td>
+          {author.firstName}
+        </td>
+        <td>
+          {author.lastName}
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
