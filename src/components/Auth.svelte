cfbd1b65949d79ff6a2cabd6d99e7b524c7a2131
m r <trash89@laposte.net>
2022-07-25 13:45:09 +0200

    cleanups

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,6 +1,5 @@
 <script>
   import { goto } from "$app/navigation";
-  //import globalStore from "../lib/globalStore";
   import { user } from "$lib/sessionStore";
   import { supabase } from "$lib/supabaseClient";
   import logo from "$lib/assets/logo-192x192.png";
@@ -11,17 +10,13 @@
   let isMember = true;
   let err;
 
-  //add alert
   $: isEmpty = !email || !password;
-  //|| $globalStore.alert;
   //toggle member
   function toggleMember() {
     isMember = !isMember;
   }
   // handle submit
   async function handleSubmit() {
-    // add alert
-    //globalStore.toggleItem("alert", true, "loading data...please wait!");
     if (isMember) {
       const { user: existingUser, error } = await supabase.auth.signIn({ email, password });
       if (error) {
@@ -39,14 +34,9 @@
       user.set(createdUser);
     }
     if ($user) {
-      //globalStore.toggleItem("alert", true, "welcome to shopping madness my friend!");
       goto("/books");
-      //globalStore.toggleItem("alert", false);
-      // add alert
       return;
     }
-    //globalStore.toggleItem("alert", true, "there was an error! please try again", true);
-    // add alert
   }
   const handleDemo = async () => {
     const { user: existingUser, error } = await supabase.auth.signIn({ email: "demo@gmail.com", password: "secret123" });
@@ -68,7 +58,7 @@
   <title>My Books Authentication</title>
 </svelte:head>
 
-<section class="container w-auto p-2 my-3 shadow border border-primary rounded-3">
+<section class="container-fluid p-2 my-3 shadow border border-primary rounded-3">
   <div class="d-flex justify-content-left align-items-center">
     <img src={logo} alt="MyBooks" width="100px" height="100px" />
     <p class="h1 text-capitalize">My Books</p>
