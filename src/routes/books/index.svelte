3b0c0c3b9e1b6c092a583367686a02b26a27973c
m r <trash89@laposte.net>
2022-07-26 19:59:29 +0200

    add csv export

--- src/routes/books/index.svelte
+++ src/routes/books/index.svelte
@@ -31,6 +31,51 @@
     }
   };
 
+  const escapeCsvCell = (cell) => {
+    if (cell == null) {
+      return "";
+    }
+    const sc = cell.toString().trim();
+    if (sc === "" || sc === '""') {
+      return sc;
+    }
+    if (sc.includes('"') || sc.includes(",") || sc.includes("\n") || sc.includes("\r")) {
+      return '"' + sc.replace(/"/g, '""') + '"';
+    }
+    return sc;
+  };
+
+  const makeCsvData = (columns, data) => {
+    return data.reduce((csvString, rowItem) => {
+      return csvString + columns.map(({ accessor }) => escapeCsvCell(accessor(rowItem))).join(",") + "\r\n";
+    }, columns.map(({ name }) => escapeCsvCell(name)).join(",") + "\r\n");
+  };
+
+  const downloadAsCsv = (columns, data, filename) => {
+    const csvData = makeCsvData(columns, data);
+    const csvFile = new Blob([csvData], { type: "text/csv" });
+    const downloadLink = document.createElement("a");
+
+    downloadLink.display = "none";
+    downloadLink.download = filename;
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  const handleDownloadCsv = () => {
+    const columns = [
+      { accessor: (item) => item.firstName, name: "FirstName" },
+      { accessor: (item) => item.lastName, name: "LastName" },
+      { accessor: (item) => item.name, name: "Book Name" },
+      { accessor: (item) => item.langue, name: "Langue" },
+      { accessor: (item) => item.coll_name, name: "Collection" },
+    ];
+
+    downloadAsCsv(columns, tableData, "MyBooks");
+  };
+
   onMount(async () => {
     await getAllRows();
   });
@@ -43,7 +88,12 @@
 
 <section class="container p-2 my-2 border border-primary rounded-3">
   <p class="h4 text-capitalize">books</p>
-  <a href={`${route}/newRow`}><i class="fa-solid fa-plus" /></a>
+  <div class="d-flex justify-content-between">
+    <a href={`${route}/newRow`}><i class="fa-solid fa-plus" /></a>
+    <button type="button" class="btn btn-outline-light text-dark flex-fill" data-bs-toggle="tooltip" title="Export to CSV" on:click={handleDownloadCsv}
+      ><i class="fa-solid fa-file-csv" /></button
+    >
+  </div>
   <div class="table-responsive">
     <table class="table table-sm table-bordered table-hover">
       <thead>
